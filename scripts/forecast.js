f3f02d9707904ad8fac09404aa3d521e96da163f
raineldn <raineldn@gmail.com>
2019-05-22 14:06:16 +0100

    Added Locations API

---
+++ scripts/forecast.js
@@ -0,0 +1,16 @@
+const key = 'LdPMSvM0hRfRnm4W4h6I1x3juQbwCz7W';
+
+const getCity = async (city) => {
+
+	const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+	const query = `?apikey=${key}&q=${city}`;
+
+	const response = await fetch(base + query);
+	const data = await response.json();
+
+	return data[0];
+}
+
+getCity('London')
+	.then(data => console.log(data))
+	.catch(err => console.log(err));
